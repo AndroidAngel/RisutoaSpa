c0977d23fdc3e24502ff0b79a2d049af8b6e6383
iamangelauditor <iamangelauditor@gmail.com>
2017-11-10 09:42:08 +0800

    First Commit

---
+++ app/src/main/java/com/teamandroidangel/iamangelauditor/risutoaspa/MainActivity.java
@@ -0,0 +1,13 @@
+package com.teamandroidangel.iamangelauditor.risutoaspa;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
